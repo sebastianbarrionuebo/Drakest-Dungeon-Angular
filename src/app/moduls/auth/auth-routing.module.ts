7c411ea9addf26e45b93f5ae474441e0dd0f1822
Barrionuebo Sebastian <seb.barrionuebo@gmail.com>
2023-11-14 19:41:03 -0300

    correcciones

--- src/app/moduls/auth/auth-routing.module.ts
+++ src/app/moduls/auth/auth-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-//import { loginGuard } from 'src/app/core/services/auth/guards/login.guard';
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent,
-    //canActivate: [loginGuard]
+    component: LoginComponent
   },
   {
     path: 'register',
